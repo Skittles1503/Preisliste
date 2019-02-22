83af4b215e82a2eb28055bc283c56cc1934b7e2c
Skittles1503 <47594799+Skittles1503@users.noreply.github.com>
2019-02-22 11:16:04 +0100

    s

--- c.php
+++
@@ -1,8 +0,0 @@
-
-<base href="https://www.chemnitz.de/">
-<?php
-    $html = file_get_contents('https://www.chemnitz.de/');
-    $html = str_replace("Aktuelles","Hallo Welt",$html);
-    print $html;
-    
-?>
